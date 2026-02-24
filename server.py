aa2551d5bf8572189494fe928f9c15255856b5b8
Filip Drobnjak <tvojemail@example.com>
2026-02-24 23:34:47 +0100

    flask app

--- server.py
+++ server.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import smtplib
 from email.mime.text import MIMEText
 import os
+import sqlite3
+
+
 
 app = Flask(__name__)
-submissions = []
+
+def init_db():
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS korisnici(
+          id INTEGER PRIMARY KEY,
+          first_name TEXT NOT NULL,
+          last_name TEXT NOT NULL,
+          email TEXT NOT NULL,
+          telephone TEXT)
+
+          """)
+    conn.commit()
+    conn.close()
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -13,21 +31,31 @@ def index():
         first_name = request.form.get("first_name")
         last_name = request.form.get("last_name")
         email = request.form.get("email")
-        phone = request.form.get("telephone")
+        telephone = request.form.get("telephone")
+        
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute("""
+                INSERT INTO korisnici (first_name, last_name, email, telephone)
+                VALUES (?,?,?,?)
+            """, (first_name, last_name, email, telephone))
+        conn.commit()
+        conn.close()
 
-        submissions.append({
-            "first_name": first_name,
-            "last_name": last_name,
-            "email": email,
-            "phone": phone                    
-        })
 
-        send_email(email, first_name, last_name, phone)
+        send_email(email, first_name, last_name, telephone)
 
         message = "Успешно сте се пријавили!"
+    
+        return redirect(url_for("success"))
+    
     return render_template("index.html", message=message)
 
-def send_email(to_email, first_name, last_name, phone):
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+def send_email(email, first_name, last_name, phone):
     sender_email = "drobnjakfilip0@gmail.com"
     sender_password = os.environ.get("EMAIL_PASSWORD")
     subject = "Потврда пријаве"
@@ -36,17 +64,13 @@ def send_email(to_email, first_name, last_name, phone):
     msg = MIMEText(body)
     msg["Subject"] = subject
     msg["From"] = sender_email
-    msg["To"] = to_email
+    msg["To"] = email
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(sender_email, sender_password)
         server.send_message(msg)
 
-@app.route("/submissions")
-def show_submissions():
-    return {"submissions": submissions}  # Flask će ovo vratiti kao JSON
-
-
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    init_db()
+    app.run(debug=True, port=5500)
